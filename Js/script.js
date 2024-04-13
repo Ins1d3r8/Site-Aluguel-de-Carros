7be3c33ca2d2ef93676eda5753763a3280e0b57d
Christian <joaochristian03@gmail.com>
2024-04-13 08:59:51 -0300

    Primeiras alterações na Box de inscrição

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+
+
+function toggleOpcoesAluguel() {
+    var opcoesAluguel = document.getElementById('opcoesAluguel');
+    var alugarVeiculosButton = document.getElementById('alugarVeiculosButton')
+    var body = document.body;
+
+    if (opcoesAluguel.style.opacity === '1') {
+        opcoesAluguel.style.opacity = '0';
+        body.style.overflowY = 'auto';
+    } else {
+        opcoesAluguel.style.opacity = '1';
+        body.style.overflowY = 'hidden';
+        alugarVeiculosButton.style.display = 'none'
+
+    }
+}
+
+function continuarAluguel() {
+    alert('AGUARDE ENQUANTO CARREGAMOS SUAS INFORMAÇÕES...');
+}
